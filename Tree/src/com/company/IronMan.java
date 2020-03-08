488ba7ddacb98d1523b9a6d9a7c5e746584468ac
พรรณนิภา ผลเจริญ <58881202+Parnnipa86@users.noreply.github.com>
2020-03-08 23:47:23 +0700

    623410007-0

---
+++ Tree/src/com/company/IronMan.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class IronMan {
+    String model;
+    String color;
+    String manufacturing_company;
+    String Mask;
+    void CanMoveLimes(){}
+    void OpenMask(){}
+    void Show(){}
+    void Playable(){}
+  }
+    class IronManGrey
+    {
+    String model;
+    String color;
+    String manufacturing_company;
+    String Mask;
+    void CanMoveLimes(){}
+    void OpenMask(){}
+    void Show(){}
+    void Playable(){}
+}
+class IronManBlackGold
+{
+    String model;
+    String color;
+    String manufacturing_company;
+    String Mask;
+    void CanMoveLimes(){}
+    void OpenMask(){}
+    void Show(){}
+    void Playable(){}
+}
+class IronManRed
+{
+    String model;
+    String color;
+    String manufacturing_company;
+    String Mask;
+    void CanMoveLimes(){}
+    void OpenMask(){}
+    void Show(){}
+    void Playable(){}
+}
+
---
+++ Tree/src/com/company/TreeObject.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TreeObject {
+    public class Tree
+    {
+        String name;
+        String age;
+        String Use;
+        String GardenStyle;
+        void photosynthesis(){}
+        void SpitoutOxygen(){}
+        void Shade(){}
+        void AbsorbCarbondioxide(){}
+    }
+}
